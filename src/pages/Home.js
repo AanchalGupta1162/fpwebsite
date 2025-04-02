7c4d55b6f7272d05b37e486a6537fedaface633c
XohamOG <sohampatilarun11@gmail.com>
2025-04-03 00:42:50 +0530

    chatbot added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import ColorSchemesExample from '../components/Navbar'
-import UncontrolledExample from '../components/Carousel'
+import React from "react";
+import ColorSchemesExample from "../components/Navbar";
+import UncontrolledExample from "../components/Carousel";
+import AvatarChatbot from "../components/AvatarChatbot"; // Import the chatbot
 
 const Home = () => {
   return (
-    <div><ColorSchemesExample/>
     <div>
-        <UncontrolledExample/>
+      <ColorSchemesExample />
+      <div>
+        <UncontrolledExample />
+      </div>
+
+      {/* Add the Avatar Chatbot Component */}
+      <AvatarChatbot />
     </div>
-    </div>
-    
-  )
-}
+  );
+};
 
 export default Home;
